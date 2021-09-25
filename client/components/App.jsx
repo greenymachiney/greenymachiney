21749db1fa61dbfd91f0941bff692475afe0f2ab
emmapejko <emma.pejko@gmail.com>
2021-09-25 17:22:00 -0500

    fix: pushing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
+  HashRouter,
   Switch,
   Route,
   Link
 } from "react-router-dom";
 import axios from 'axios';
 
+import Home from './Home.jsx';
+import Profile from './Profile.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,21 +19,33 @@ class App extends Component {
 
   }
 
-
-
   render() {
     return (
-    <Router>
       <div>
         <h1>Drink Dat</h1>
-        <div>
+        {/* <div>
           <a href='/auth/google'>Google Log In</a>
         </div>
         <div>
           <a href='/auth/logout'>Google Log Out</a>
-        </div>
-      </div>
-    </Router>
+        </div> */}
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/profile" component={Profile}>
+            {/* <Profile /> */}
+          </Route>
+          <Route path='/users/:username'>
+            <div>logged in</div>
+          </Route>
+          <Route>
+            <div>404 page not available</div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
     )
   }
 }
