19dd8ba6cc23d3e7f7e1fccdaa24442c46d9bd50
emmapejko <emma.pejko@gmail.com>
2021-09-27 11:04:49 -0500

    feat: api call and routes

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
 
   return (
     <div>
       search for a new drink recipe
+
+      <button onClick={getRandomCocktail}>
+        Get a Random Cocktail
+      </button>
     </div>
   )
 }
