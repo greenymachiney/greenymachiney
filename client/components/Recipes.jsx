c15f1995bde4af474204b1091caa8404c248c09d
Jacob Young <jyoung3128@gmail.com>
2021-09-28 10:29:56 -0500

    dynamically render fav drinks

--- client/components/Recipes.jsx
+++ client/components/Recipes.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from 'axios';
-import Search from './Search.jsx'
 class Recipes extends React.Component{
   constructor(props){
     super(props);
@@ -8,18 +7,29 @@ class Recipes extends React.Component{
       drinks: [],
     };
     this.getSavedDrinks = this.getSavedDrinks.bind(this);
+    this.getDrinkData = this.getDrinkData.bind(this);
   }
 
 
 
   getSavedDrinks() {
     axios.get('/drunk/drinks')
-      .then(({ data }) => console.log(data, 'my fav drink'))
+      .then(({ data }) => this.setState({drinks: data}))
       .catch(err => console.error(err));
   }
 
+  getDrinkData() {
+    const { drinks } = this.state;
+    axios.get(`cocktailByName/${drinks}`)
+      .then(({ data }) => console.log(data[0], 'drink data!'))
+      .catch(err => console.error(err));
+  }
+
+
+
   componentDidMount(){
-    console.log(this.getSavedDrinks());
+    this.getSavedDrinks();
+    this.getDrinkData();
   }
 
 
@@ -30,19 +40,17 @@ class Recipes extends React.Component{
     <div className="list-group">
       <h1 className='drinkBookHeader'>Drink Book</h1>
     {
-    !this.getSavedDrinks.length ? null : getSavedDrinks().map((drink, i) => (
-      <div>{drinks}</div>
-
+    drinks.map((drink, i) => (
+      <a href="#" className="list-group-item list-group-item-action" aria-current="true" key={i}>
+      <div className="d-flex w-100 justify-content-between" key={i}>
+        <h5 className="mb-1" key={i}>{drink}</h5>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/MargaritaReal.jpg/1200px-MargaritaReal.jpg" width="100" height="100"></img>
+      </div>
+      <p className="mb-1">Some placeholder content in a paragraph.</p>
+      <small>And some small print.</small>
+    </a>
     ))
     }
-  <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Drink 1</h5>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/MargaritaReal.jpg/1200px-MargaritaReal.jpg" width="100" height="100"></img>
-    </div>
-    <p className="mb-1">Some placeholder content in a paragraph.</p>
-    <small>And some small print.</small>
-  </a>
 </div>
   )
   }
