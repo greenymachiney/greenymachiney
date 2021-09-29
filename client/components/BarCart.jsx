cd9ffedf3989c8999db28c8349bbbf03751e3f4c
Luke Johnson <benjaminlukejohnson@gmail.com>
2021-09-29 16:31:00 -0500

    added styling

--- client/components/BarCart.jsx
+++ client/components/BarCart.jsx
@@ -74,6 +74,11 @@ saveDrink() {
   axios.post('/drunk/saveCocktail', {drink: this.state.drink})
   .then(()=> console.log('saved!'))
   .catch(err => console.error(err));
+
+  const addToShoppingList = (ingredient) => {
+    axios.post('/shopping/addItem', { item: ingredient })
+      .catch(err => console.error(err));
+  }
 }
 
 componentDidMount() {
@@ -93,20 +98,29 @@ componentDidMount() {
             <div><img src={drink.strDrinkThumb} height='300px'></img></div>
             <div>
               Ingredients:
-              <ul>
+              <div>
                 {
-                  this.getIngredients().map((ingredient, i) => <li key={i}>{drink[`strMeasure${i + 1}`]} {ingredient}</li>)
+                  this. getIngredients().map((ingredient, i) => <div key={i}>
+                      <button onClick={() => addToShoppingList(ingredient)} className="btn btn-success btn-sm add-ingredient-button" title="add to your shopping list!">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="2 2 16 16">
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                  </button>
+                  {drink[`strMeasure${i + 1}`]} {ingredient}
+                
+                </div>)
                 }
-              </ul>
+              </div>
             </div>
             <div>
               Instructions:
               <div>{drink.strInstructions}</div>
             </div>
 
-            <div>
-              <button onClick={this.saveDrink}>
-                Save this drink to your drink book!
+            <div className="save-drink-button">
+              <button className="btn btn-success btn-sm" onClick={this.saveDrink}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                </svg>
               </button>
             </div>
 
@@ -127,14 +141,14 @@ componentDidMount() {
         </div>
         {
             !this.state.drinks.length ? null :
-            <div>
+            <div className="list-group">
               <ul>
                 {
                   this.state.drinks.map((drink, i) => (
                     <li key={i}  onClick={() => {
                         const drinkObj = this.getCocktailByExactName(drink.strDrink)
                         this.setState({drink: {drinkObj}})
-                      }}>{drink.strDrink}</li>
+                      }} type="button" className="list-group-item list-group-item-action" aria-current="true" >{drink.strDrink}</li>
                   ))
                 }
               </ul>
