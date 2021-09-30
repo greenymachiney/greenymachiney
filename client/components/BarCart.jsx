22066db45460708fae13fb81cf323d1b8d5cf0d4
Luke Johnson <benjaminlukejohnson@gmail.com>
2021-09-30 15:46:46 -0500

    added styling to all headers and lists

--- client/components/BarCart.jsx
+++ client/components/BarCart.jsx
@@ -89,7 +89,9 @@ componentDidMount() {
   const {drink} = this.state
   return (
       <div>
-        <h1 className='drinkBookHeader'>Bar Cart</h1>
+        <div className='bar-cart-header'>
+        <h1 >Bar Cart</h1>
+        </div>
         <div>
         {
           !drink.strDrink ? null :
@@ -138,12 +140,15 @@ componentDidMount() {
           <div className="shopping-list">
          {
          this.state.liquorList.map((liquor, index) => (
-              <button key={index} className="list-group-item list-group-item-action" onClick={() => this.getSpecificLiquorRecipes(liquor)} >{liquor}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16" onClick={() => this.deleteLiquor(liquor)}>
+           <button key={index}
+              className="list-group-item list-group-item-action"> 
+              <span onClick={() => this.getSpecificLiquorRecipes(liquor)}>{liquor}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16" onClick={() => this.deleteLiquor(liquor)}>
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
               </svg>
               </button>
+              
          ))
          }
         </div>
