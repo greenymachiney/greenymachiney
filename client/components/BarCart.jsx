08c29580a1192fdd4a5cd7a13281fdb3733fc74b
emmapejko <emma.pejko@gmail.com>
2021-09-30 15:16:11 -0500

    fix: some bugs

--- client/components/BarCart.jsx
+++ client/components/BarCart.jsx
@@ -18,6 +18,7 @@ class BarCart extends React.Component {
     this.getIngredients = this.getIngredients.bind(this);
     this.saveDrink = this.saveDrink.bind(this);
     this.deleteLiquor = this.deleteLiquor.bind(this);
+    this.addToShoppingList = this.addToShoppingList.bind(this);
   }
 
 handleClick() {
@@ -70,10 +71,11 @@ saveDrink() {
   .then(()=> console.log('saved!'))
   .catch(err => console.error(err));
 
-  const addToShoppingList = (ingredient) => {
-    axios.post('/shopping/addItem', { item: ingredient })
-      .catch(err => console.error(err));
-  }
+}
+
+addToShoppingList(ingredient) {
+  axios.post('/shopping/addItem', { item: ingredient })
+    .catch(err => console.error(err));
 }
 
 componentDidMount() {
@@ -103,7 +105,7 @@ componentDidMount() {
               <div>
                 {
                   this.getIngredients().map((ingredient, i) => <div key={i} className='ingredient-item'>
-                      <button onClick={() => addToShoppingList(ingredient)} className="btn btn-success btn-sm add-ingredient-button" title="add to your shopping list!">
+                      <button onClick={() => this.addToShoppingList(ingredient)} className="btn btn-success btn-sm add-ingredient-button" title="add to your shopping list!">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="2 2 16 16">
                       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                     </svg>
