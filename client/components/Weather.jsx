4bcf6ddf0cc77d432d41eac65ecd2420375ecbe4
John Dyer <jldyer85@gmail.com>
2022-04-07 17:26:56 -0500

    axios req in weather.jsx returning geolocation of current weather

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+const key = require('../../config/keys').api.weather;
+
 
 const Weather = () => {
   const [weather, setWeather] = useState({});
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
 
   const getWeather = () => {
-    axios
-      .get(`/weather`)
-      .then(({ data }) => {
-        setWeather(data);
-      })
-      .catch((err) => console.error(err));
+
   };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+        // console.log("Latitude is:", lat)
+        // console.log("Longitude is:", long)
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
+      .then(res => res)
+      .then(result => {
+        result;
+        console.log('LINE 25: ', result); //returns object of current weather data from geolocation lat/lon
+      })
+  }, [lat, lon]);
 
   return (
     <>
       <button
         onClick={getWeather}
-        className="btn btn-success btn-sm"
+        className="btn btn-weather btn-sm"
         title="get weather"
       >
         Check Weather
