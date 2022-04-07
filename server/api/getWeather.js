f18a54ed213270164fb2b726efb23beb1a70a8cf
John Dyer <jldyer85@gmail.com>
2022-04-07 14:43:42 -0500

    add imperial measurement of weather to api call

--- server/api/getWeather.js
+++ server/api/getWeather.js
@@ -3,8 +3,8 @@ const key = require('../../config/keys').api.weather;
 
 const getCurrentWeather = (lat, lon) => {
   return axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
-    .then((response) => response.data)
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
+    .then((response) => console.log(response.data)) //returns data object of current weather conditions
     .catch((err) => console.error('error in weather api call: ', err));
 };
 
