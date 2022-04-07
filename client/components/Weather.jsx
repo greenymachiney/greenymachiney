82c2627448bdd1a9b4647772ed7a194fc89dbb83
John Dyer <jldyer85@gmail.com>
2022-04-07 18:20:25 -0500

    created weathercard.jsx

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// import WeatherCard from './components/WeatherCard.jsx';
 const key = require('../../config/keys').api.weather;
+
 
 
 const Weather = () => {
@@ -8,28 +10,23 @@ const Weather = () => {
   const [lat, setLat] = useState([]);
   const [lon, setLon] = useState([]);
 
-  const getWeather = () => {
-
-  };
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function(position) { //returns lat/lon based on user location
       setLat(position.coords.latitude);
       setLon(position.coords.longitude);
     });
-        // console.log("Latitude is:", lat)
-        // console.log("Longitude is:", long)
       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
       .then(res => res)
       .then(result => {
-        result;
-        console.log('LINE 25: ', result); //returns object of current weather data from geolocation lat/lon
+        setWeather(result.data);
+        console.log('LINE 25: ', result.data); //returns object of current weather data from geolocation lat/lon
       })
   }, [lat, lon]);
 
   return (
     <>
       <button
-        onClick={getWeather}
+        onClick={() => weather}
         className="btn btn-weather btn-sm"
         title="get weather"
       >
