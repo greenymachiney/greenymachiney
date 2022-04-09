1aa1ca47b4a6ab52ad3a1911ad3c6831f9c154b7
John Dyer <jldyer85@gmail.com>
2022-04-09 12:11:58 -0500

    changed server side weather routes to return data by city name

--- server/api/getWeather.js
+++ server/api/getWeather.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 const key = require('../../config/keys').api.weather;
 
-const getCurrentWeather = (lat, lon) => {
+const getCurrentWeather = (name) => {
   return axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
-    .then((response) => response.data) //returns data object of current weather conditions
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${key}&units=imperial`)
+    .then((response) => console.log('WEATHER API', response.data)) //returns data object of current weather conditions based on city;
     .catch((err) => console.error('error in weather api call: ', err));
 };
 
